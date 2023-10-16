57695f5c4fbf0d0908bdd55e18542c355706b8d2
Passawut <passawut.prinyagul@gmail.com>
2023-10-16 21:36:30 +0700

    add functions for shooting and confirming shoot

--- projects/battleship/battleship/client/view/games.py
+++ projects/battleship/battleship/client/view/games.py
@@ -1,6 +1,6 @@
 from tgraphics.color import colors
 from tgraphics.component import Component, Window
-from tgraphics.reactivity import ComputedFuture, computed, unref, Ref
+from tgraphics.reactivity import computed, unref, Ref
 from tsocket.shared import Empty
 
 from .. import store
@@ -9,22 +9,34 @@ from ..client import BattleshipClient
 
 @Component.register("Games")
 def games_ui(window: Window, client: BattleshipClient, **kwargs):
-    gap_size = 1
-    grid_row = [str(i) for i in range(1, 9)[::-1]]
-    # [8,7,6,5,4,3,2,1]
-    grid_column = [chr(i + 97) for i in range(8)]
-    # [a,b,c,d,e,f,g,h]
-
+    gap_size = 10
+    grid_row = [chr(i + 97) for i in range(8)[::-1]]
+    grid_column = [str(i) for i in range(1, 9)]
+    skill_activated = Ref("False")
     battleship_state = {}
-    for the_row in grid_row:
-        for the_column in grid_column:
-            battleship_state[the_column + the_row] = Ref("white")
+    for row in grid_row:
+        for column in grid_column:
+            battleship_state[(column,row)] = Ref("white")
     # Starting board
 
-    def select_shooting_spot(position):
-        if unref(battleship_state[position]) == "white":
-            battleship_state[position] = "green"
-
+    async def select_shooting_spot(e_):
+        async def check_position(column, row):
+            if unref(skill_activated) == "False":
+                if unref(battleship_state[(column, row)]) == "white":
+                    battleship_state[(column, row)].value = "green"
+            elif unref(skill_activated) == "True":
+                # A skill that bomb a row
+                for each_column in range(1,9):
+                    if battleship_state[(column, each_column)] != "brown":
+                        battleship_state[(column, each_column)].value = "green"
+    async def select_skill(e_):
+        skill_activated.value = "True"
+    async def confirm_shoot(e_):
+        async def check_hit():
+            pass
+        for position, color in battleship_state.items():
+            if unref(color) == "green":
+                pass
     return Component.render_xml(
         """
         <Column gap="16" width="window.width" height="window.height">
@@ -32,13 +44,13 @@ def games_ui(window: Window, client: BattleshipClient, **kwargs):
                 <Row t-for="row in grid_row" gap="gap_size">
                     <LabelButton 
                         t-for="column in grid_column"
-                        text="column+row" 
-                        text_color="colors[unref(battleship_state[column+row])]"
+                        text="row+column" 
+                        text_color="colors[unref(battleship_state[(column,row)])]"
                         color="colors['white']"
-                        hover_color="colors['green']"
+                        hover_color="colors['white']"
                         width="1"
                         height="1"
-                        handle-ClickEvent="select_shooting_spot(column+row)"
+                        handle-ClickEvent="select_shooting_spot"
                     />
                 </Row>
             </Column>
