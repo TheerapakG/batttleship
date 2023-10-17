e6103cb21350d675f6f03131d2c4c8b9f7d163b9
Passawut <passawut.prinyagul@gmail.com>
2023-10-17 23:51:51 +0700

    change state to nested list

--- projects/battleship/battleship/client/view/games.py
+++ projects/battleship/battleship/client/view/games.py
@@ -1,46 +1,54 @@
+
 from tgraphics.color import colors
 from tgraphics.component import Component, Window
 from tgraphics.reactivity import computed, unref, Ref
 from tsocket.shared import Empty
 
+
 from .. import store
 from ..client import BattleshipClient
-
+from ..component.button import ClickEvent
 
 @Component.register("Games")
 def games_ui(window: Window, client: BattleshipClient, **kwargs):
-    gap_size = 10
-    grid_row = [chr(i + 97) for i in range(8)[::-1]]
-    grid_column = [str(i) for i in range(1, 9)]
+    gap_size = 16
+    grid_row = [i for i in range(8)]
+    grid_column = [i for i in range(8)]
     skill_activated = Ref("False")
-    battleship_state = {}
-    for row in grid_row:
-        for column in grid_column:
-            battleship_state[(column, row)] = Ref("white")
+    target_choosen = Ref("False")
+    battleship_state = []
+    color_state = []
     # Starting board
+    for row in range(8):
+        battleship_state.append([])
+        for column in range(8):
+            battleship_state[row].append(Ref("Normal Space"))
+    for row in range(8):
+        battleship_state.append([])
 
-    async def select_shooting_spot(e_):
-        async def check_position(column, row):
+    def select_shooting_spot(position: (int, int)):
+        def shooting(event: ClickEvent):
+            (column, row) = position
+            if unref(target_choosen) == "True":
+                pass
             if unref(skill_activated) == "False":
-                if unref(battleship_state[(column, row)]) == "white":
-                    battleship_state[(column, row)].value = "green"
+                if unref(battleship_state[row][column]) == "Normal Space":
+                    battleship_state[row][column].value = "Choosen Space"
+                print(battleship_state)
             elif unref(skill_activated) == "True":
                 # A skill that bomb a row
-                for each_column in range(1, 9):
-                    if battleship_state[(column, each_column)] != "brown":
-                        battleship_state[(column, each_column)].value = "green"
-
-    async def select_skill(e_):
+                for each_column in range(8):
+                    if unref(battleship_state[row][each_column]) != "Environment Space":
+                        battleship_state[row][each_column].value = "Choosen Space"
+    def select_skill(e_):
         skill_activated.value = "True"
-
     async def confirm_shoot(e_):
         async def check_hit():
             pass
-
-        for position, color in battleship_state.items():
-            if unref(color) == "green":
-                pass
-
+        for state in battleship_state:
+            for values in state:
+                if unref(values) == "Choosen Space":
+                    pass
     return Component.render_xml(
         """
         <Column gap="16" width="window.width" height="window.height">
@@ -48,13 +56,13 @@ def games_ui(window: Window, client: BattleshipClient, **kwargs):
                 <Row t-for="row in grid_row" gap="gap_size">
                     <LabelButton 
                         t-for="column in grid_column"
-                        text="row+column" 
-                        text_color="colors[unref(battleship_state[(column,row)])]"
+                        text="str(row)+str(column)" 
+                        text_color="colors['white']"
                         color="colors['white']"
                         hover_color="colors['white']"
-                        width="1"
-                        height="1"
-                        handle-ClickEvent="select_shooting_spot"
+                        width="4"
+                        height="4"
+                        handle-ClickEvent="select_shooting_spot((column, row))"
                     />
                 </Row>
             </Column>
