cf4965bba8a35073bb434aad912b55af910a0fb6
Passawut <passawut.prinyagul@gmail.com>
2023-11-08 01:50:42 +0700

    feat: add volume setting and set up bgm, but haven't set up sfx

--- projects/battleship/battleship/client/view/settings.py
+++ projects/battleship/battleship/client/view/settings.py
@@ -22,15 +22,16 @@ def settings(**kwargs):
             return colors["red"][500]
 
     async def increase_volume(_e):
-        default_volume.value = unref(default_volume) + 1
-        store.bgm.bgm_volume.value = unref(default_volume) / 10
-        store.bgm.set_volume(unref(default_volume) / 10)
+        if (volume := unref(default_volume)) is not None:
+            default_volume.value = volume + 1
+            store.bgm.bgm_volume.value = volume / 10
+            store.bgm.set_volume(volume / 10)
 
     async def decrease_volume(_e):
-        if (client := unref(store.ctx.client)) is not None:
-            default_volume.value = unref(default_volume) - 1
-            store.bgm.bgm_volume.value = unref(default_volume) / 10
-            store.bgm.set_volume(unref(default_volume) / 10)
+        if (volume := unref(default_volume)) is not None:
+            default_volume.value = volume - 1
+            store.bgm.bgm_volume.value = volume / 10
+            store.bgm.set_volume(volume / 10)
 
     async def page_return(_e):
         from .main_menu import main_menu
